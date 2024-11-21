94e56c6d280a999d3eb33236e5ae5b35306aa766
Ahmed-ElGoharyy <ahmdgohary@gmail.com>
2024-11-21 21:34:40 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace trefle888.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
