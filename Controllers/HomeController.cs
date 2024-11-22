f490c229cc71f0f2c864bb3223563c0f406af9a6
Karim amr <karimamr20055@gmail.com>
2024-11-22 06:03:45 +0200

    login

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -40,6 +40,12 @@ namespace trefle888.Controllers
 
         public IActionResult Cart()
         {
+            return View();    
+
+        }
+
+        public IActionResult Login()
+        {
             return View();
         }
 
