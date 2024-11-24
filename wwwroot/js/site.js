6e312e9123c7f83694a4f6fc9be3255df677f9f4
Ahmed-ElGoharyy <ahmdgohary@gmail.com>
2024-11-24 02:40:13 +0200

    transition added

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,21 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+const logoLink = document.getElementById('homeLogoButton');
+const transitionPage = document.getElementById('transitionPage');
+
+logoLink.addEventListener('click', (event) => {
+    event.preventDefault(); // Prevent default link behavior
+
+    transitionPage.classList.remove('hidden');
+
+    setTimeout(() => {
+     
+        // Redirect to the homepage after the transition
+        window.location.href = "/";
+
+    }, 1500); // Adjust the delay as needed
+ 
+});
+
