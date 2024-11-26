63ed255e0caedc7bc9669c23e72e1879c5a3d411
Hagras99 <mostafahagras79@gmail.com>
2024-11-27 00:28:13 +0200

    try

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using trefle888.Models;
+
+namespace trefle888.Controllers
+{
+    public class ShopController : Controller
+    {
+        public IActionResult Shop()
+        {
+            var products = new List<Product>
+            {
+                new Product(1,"REGULAR FIT TRAVELER BLAZER", 6599, "/resources/suit1.jpg"),
+                new Product(2,"REGULAR FIT PREMIUM WOOL DOUBLE BREASTED BLAZER", 9740, "/resources/suit2.jpg"),
+                new Product(3,"REGULAR FIT PREMIUM WOOL BLAZER", 8250, "/resources/suit3.jpg")
+            };
+
+
+            return View(products);
+        }
+    }
+}
+
---
+++ Models/productDbcontext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace trefle888.Models
+{
+    public class ProductDbcontext : Dbcontext
+    {
+        public ProductDbcontext() : base("productConnectionString")
+        {
+
+        }
+        public Dbset<Product> products { get; set; }
+
+    }
+}
+
