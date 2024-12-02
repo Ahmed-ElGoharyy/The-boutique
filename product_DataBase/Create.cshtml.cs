9e981568837c7499bba23e2173dd4c47838334c6
Hagras99 <mostafahagras79@gmail.com>
2024-12-02 22:32:16 +0200

    try

--- product_DataBase/Create.cshtml.cs
+++ product_DataBase/Create.cshtml.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using trefle888.Data;
 using trefle888.Models;
 
